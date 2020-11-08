015c4163a4a79a03ddd9948f4d178c83da268b4b
Sean Bigelow <sean.bigelow1@gmail.com>
2020-11-08 21:33:57 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -13,9 +13,9 @@ def shipping_manifest
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
 the_manifest = {
-  5 => "whale bone corsets",
-  2 => "porcelain vases",
-  3 => "oil paintings"
+  "5" => "whale bone corsets",
+  "2" => "porcelain vases",
+  "3" => "oil paintings"
 }
 
 end
